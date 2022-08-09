e517464554659d69be3c28d7ed5d3660d18f7b2d
Thom Strub <thomstrub@gmail.com>
2022-08-08 21:59:44 -0700

    updates database settings

--- src/config/database.ts
+++ src/config/database.ts
@@ -2,8 +2,7 @@ import mongoose, { ConnectOptions } from 'mongoose';
 
 mongoose.connect(`${process.env.START_MONGODB}${process.env.MONGODB_USERNAME}:${process.env.MONGODB_PASSWORD}${process.env.END_MONGODB}`, { 
     useNewUrlParser: true,
-    useUnifiedTopology: true,
-    useCreateIndex: true
+    useUnifiedTopology: true
   } as ConnectOptions, () => {
       console.log("Connected to mongoose successfully")
   });
